7c90f5fc7c31ecf39df688a1881388e25926b989
henok <henokkahsay43@gmail.com>
2020-02-24 01:40:49 -0800

    Identify Even AND Odd_number

---
+++ src/FromYouTube/Identify_even_AND_odd_number.java
@@ -0,0 +1,31 @@
+package FromYouTube;
+
+public class Identify_even_AND_odd_number {
+
+
+    public static void main(String[] args) {
+
+    int[] array={1,6,3,4,8,2};
+        //{1,6,3,4,0,2}
+        //{1,6,3,4,8,7}
+    System.out.println(fun3(array));
+    }
+    public static int fun3(int[] array)
+    {
+        if (array == null || array.length <4) return 0;
+        int evenIndex = array[0], oddIndex = array[1];
+        for (int i=2; i<array.length; i++)
+        {
+            if (i%2 ==0 && evenIndex<array[i])
+                evenIndex = array[i];
+            else if (i%2 != 0 && oddIndex>array[i])
+                oddIndex = array[i];
+            else return 0;
+        }
+        return 1;
+    }
+}
+
+
+
+
