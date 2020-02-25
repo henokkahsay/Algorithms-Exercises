03f90cb95ee71bb40df313ea1c818713204e3aad
henok <henokkahsay43@gmail.com>
2020-02-25 00:05:19 -0800

    How to get largest subtraction value from the given array

---
+++ src/FromYouTube/How_toGetLargest_Subtraction.java
@@ -0,0 +1,22 @@
+package FromYouTube;
+
+public class How_toGetLargest_Subtraction {
+    public static void main(String[] args) {
+        //Possible Inputs are:
+        // {1,2,5,6,4},{12,54,8,3,41},null,{45}
+        int[] array = {1,1,1,1,1,1};
+        System.out.println(fun6(array));
+    }
+
+    public static int fun6(int[] array) {
+        if (array == null || array.length <= 1) return -1;
+        int sub = 0, retSub = 0;
+        for (int i = 0; i < array.length - 1; i++)
+            for (int j = i + 1; j < array.length; j++) {
+                sub = array[i] - array[j];
+                if (sub < 0) sub *= -1;
+                if (sub > retSub) retSub = sub;
+            }
+        return retSub;
+    }
+}
