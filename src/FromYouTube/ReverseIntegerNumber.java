8cf40f19f865109af662c4d20e03baa66b93eda6
henok <henokkahsay43@gmail.com>
2020-02-26 00:24:08 -0800

    ReverseIntegerNumber

---
+++ src/FromYouTube/ReverseIntegerNumber.java
@@ -0,0 +1,34 @@
+package FromYouTube;
+
+public class ReverseIntegerNumber {
+//Examples
+//12005
+// 1
+//1000
+// 0
+// -12345
+    public static void main(String[] args) {
+        int sampleData=100000;
+
+        int result=reverseGivenInteger(sampleData);
+
+        System.out.println(String.valueOf(result));
+    }
+    static  int reverseGivenInteger(int givenInteger){
+        int sign =1;
+        if (givenInteger == 0){
+            return 0;
+        }
+        if (givenInteger <0){
+            sign =-1;
+            givenInteger =(-1)* givenInteger;//change given negative number to positive
+
+        }
+        int output =0;
+        while (givenInteger != 0){
+            output=(output * 10)+ (givenInteger%10);
+            givenInteger=givenInteger / 10;
+        }
+        return sign* output;// change proper sign to the out put and return result
+    }
+}
