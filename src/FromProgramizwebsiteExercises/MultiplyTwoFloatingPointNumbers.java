e98c983a265c555977c4b69ac138c099a8de09ff
henok <henokkahsay43@gmail.com>
2020-02-27 01:26:36 -0800

    MultiplyTwoFloatingPointNumbers

---
+++ src/FromProgramizwebsiteExercises/MultiplyTwoFloatingPointNumbers.java
@@ -0,0 +1,12 @@
+package FromProgramizwebsiteExercises;
+
+public class MultiplyTwoFloatingPointNumbers {
+
+    public static void main(String[] args) {
+        float first =1.4f;
+        float second =3.5f;
+
+        float sum = first*second;
+        System.out.println("The product is :"+sum);
+    }
+}
