2af32509805748c279db338cf1296f12b1163a14
henok <henokkahsay43@gmail.com>
2020-02-28 00:26:17 -0800

    ToCheckEvenAndOdd_InputIsFromUserUsingScanner

--- src/FromProgramizwebsiteExercises/VowelConsonant.java
+++ src/FromProgramizwebsiteExercises/VowelConsonant.java
@@ -1,4 +1,17 @@
 package FromProgramizwebsiteExercises;
 
+import java.util.Scanner;
+
 public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Enter a Character ");
+        char ch = reader.next().charAt(0);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) {
+            System.out.println(ch + " is vowel");
+        } else
+            System.out.println(ch + " is consonant");
+
+    }
 }
