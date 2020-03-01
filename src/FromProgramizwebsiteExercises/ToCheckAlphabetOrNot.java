28ed16a31ce15571aaacf80d1391157f5d523fe1
henok <henokkahsay43@gmail.com>
2020-03-01 04:12:47 -0800

    CheckAlphabetOrNot

--- src/FromProgramizwebsiteExercises/ToCheckAlphabetOrNot.java
+++ src/FromProgramizwebsiteExercises/ToCheckAlphabetOrNot.java
@@ -2,7 +2,7 @@ package FromProgramizwebsiteExercises;
 
 public class ToCheckAlphabetOrNot {
     public static void main(String[] args) {
-        char c= '%';
+        char c= 'A';
         if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
         System.out.println(c + " is an alphabet");
         else
