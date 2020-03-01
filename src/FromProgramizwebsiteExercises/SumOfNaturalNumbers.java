ab2b5a86968590764a27f2269497f0ac4c1e99c1
henok <henokkahsay43@gmail.com>
2020-03-01 04:27:43 -0800

    SumOfNumbers

---
+++ src/FromProgramizwebsiteExercises/SumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+package FromProgramizwebsiteExercises;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int num =100, sum =0;
+        for (int i= 1; i<=num; i++)
+        {
+         // Sum =Sum + i;
+         sum +=i;
+
+        }
+        System.out.println("Sum =" + sum);
+    }
+
+}
