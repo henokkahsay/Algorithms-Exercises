e6bd9c0ace0ad4d10fdc98f17ca754229c53792e
henok <henokkahsay43@gmail.com>
2020-03-02 22:52:43 -0800

    CalculatePowerOfNumber

---
+++ src/FromProgramizwebsiteExercises/CalculatePowerOfNumber.java
@@ -0,0 +1,13 @@
+package FromProgramizwebsiteExercises;
+
+public class CalculatePowerOfNumber {
+    public static void main(String[] args) {
+        int base=3, exponent=4;
+        long result=1;
+        while (exponent !=0){
+            result*=base;
+            --exponent;
+        }
+        System.out.println("Answer =" + result);
+    }
+}
