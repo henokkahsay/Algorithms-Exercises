f9c9bf0b9b94bd683d42458a81c544cfb282bd61
henok <henokkahsay43@gmail.com>
2020-03-02 23:09:19 -0800

    ToCheck Palindrome

---
+++ src/FromProgramizwebsiteExercises/ToCheckPalidrome.java
@@ -0,0 +1,20 @@
+package FromProgramizwebsiteExercises;
+
+public class ToCheckPalidrome {
+    public static void main(String[] args) {
+        int num =121 ,reversedInteger=0,remainder,originalInteger;
+        originalInteger=num;
+        //Reversed integer is stored in variable
+        while (num!=0){
+            remainder =num%10;
+            reversedInteger=reversedInteger*10+remainder;
+            num /=10;
+
+        }
+        //Palindrome if orignalInteger and reversedInteger are equal
+        if (originalInteger==reversedInteger)
+            System.out.println(originalInteger+ " is a palindrome.");
+            else
+                System.out.println(originalInteger + "is not a palindrome.");
+    }
+}
