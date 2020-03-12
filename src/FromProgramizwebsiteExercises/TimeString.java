58eeab03249f88a35ad686957af09018707e3286
henok <henokkahsay43@gmail.com>
2020-03-12 02:11:54 -0700

    Convert String To Date Using Predefined Formatters

---
+++ src/FromProgramizwebsiteExercises/TimeString.java
@@ -0,0 +1,13 @@
+package FromProgramizwebsiteExercises;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TimeString {
+    public static void main(String[] args) {
+        //Format y-M-d or yyy-MM-d
+        String string="2017-07-25";
+        LocalDate date=LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
+        System.out.println(date);
+    }
+}
