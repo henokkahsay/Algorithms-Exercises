cfdf0fa03d6a9863964971d58ddda14b13ec30c1
henok <henokkahsay43@gmail.com>
2020-03-12 00:20:12 -0700

    Find Frequncy Of Character

---
+++ src/FromProgramizwebsiteExercises/FindFrequencyOfCharacterInString.java
@@ -0,0 +1,15 @@
+package FromProgramizwebsiteExercises;
+
+public class FindFrequencyOfCharacterInString {
+    public static void main(String[] args) {
+     String str="This Website is awesome.";
+     char ch='e';
+     int frequency=0;
+     for (int i=0; i<str.length();i++){
+         if (ch == str.charAt(i)){
+             ++frequency;
+         }
+     }
+     System.out.println("Frequency of " + ch +"="+frequency);
+    }
+}
