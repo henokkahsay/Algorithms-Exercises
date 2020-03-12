f0466334dd365a434d23f5ebab5279e330e0fc6c
henok <henokkahsay43@gmail.com>
2020-03-12 07:12:21 -0700

    To Get Current Working Directory

---
+++ src/FromProgramizwebsiteExercises/CurrDirectory.java
@@ -0,0 +1,8 @@
+package FromProgramizwebsiteExercises;
+
+public class CurrDirectory {
+    public static void main(String[] args) {
+        String path=System.getProperty("user.dir");
+        System.out.println("Working Directory = " + path);
+    }
+}
