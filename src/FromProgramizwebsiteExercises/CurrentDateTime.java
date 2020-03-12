cec8dc8eb5a22dbe4b450f2965355ebaf20963c7
henok <henokkahsay43@gmail.com>
2020-03-12 02:54:00 -0700

    Get Current date and time in default foramt

---
+++ src/FromProgramizwebsiteExercises/CurrentDateTime.java
@@ -0,0 +1,11 @@
+package FromProgramizwebsiteExercises;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class CurrentDateTime {
+    public static void main(String[] args) {
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println("Current Date and Time is."+ current);
+    }
+}
