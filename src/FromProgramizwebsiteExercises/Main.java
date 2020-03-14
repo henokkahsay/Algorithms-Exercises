634bf5f4df097d4bfbeb7384b60c3a30235dc055
henok <henokkahsay43@gmail.com>
2020-03-14 03:34:27 -0700

    Create a new directory in java

---
+++ src/FromProgramizwebsiteExercises/Main.java
@@ -0,0 +1,19 @@
+package FromProgramizwebsiteExercises;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        // creates a file object with specified path
+        File file=new File("Java Example\\directory");
+
+        // tries to create a new directory
+        boolean value = file.mkdir();
+        if (value){
+            System.out.println("The new directory is created.");
+
+        }else {
+            System.out.println("The directory already exists.");
+        }
+    }
+}
