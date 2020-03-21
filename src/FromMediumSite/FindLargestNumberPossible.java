23dd192c5a4dcc866f52518b2e23a5242a19103d
henok <henokkahsay43@gmail.com>
2020-03-21 01:21:00 -0700

    7 Algorithms work

--- src/FromMediumSite/FindLargestNumberPossible.java
+++ src/FromMediumSite/FindLargestNumberPossible.java
@@ -1,4 +1,17 @@
 package FromMediumSite;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class FindLargestNumberPossible {
+    public static void main(String[] args)
+        {
+            List<String> numbers = Arrays.asList("10", "68", "97", "9", "21", "12");
+
+            // sort using a custom function object
+            Collections.sort(numbers, (a, b) -> (b + a).compareTo(a + b));
+
+            // print the sorted sequence
+            numbers.stream().forEach(System.out::print);
+        }
 }
--- src/FromMediumSite/SubarrayHavingGivenSum.java
+++ src/FromMediumSite/SubarrayHavingGivenSum.java
@@ -1,4 +1,48 @@
 package FromMediumSite;
 
 public class SubarrayHavingGivenSum {
+        // Function to print sub-array having given sum using
+        // sliding window technique
+        public static void findSubarray(int[] A, int sum)
+        {
+            // maintains sum of current window
+            int windowSum = 0;
+
+            // maintain a window [low, high-1]
+            int low = 0, high = 0;
+
+            // consider every sub-array starting from low index
+            for (low = 0; low < A.length; low++)
+            {
+                // if current window's sum is less than the given sum,
+                // then add elements to current window from right
+                while (windowSum < sum && high < A.length)
+                {
+                    windowSum += A[high];
+                    high++;
+                }
+
+                // if current window's sum is equal to the given sum
+                if (windowSum == sum)
+                {
+                    System.out.printf("Subarray found [%d-%d]\n", low, high - 1);
+                    return;
+                }
+
+                // At this point the current window's sum is more than the given
+                // sum remove current element (leftmost element) from the window
+                windowSum -= A[low];
+            }
+        }
+
+        // main function
+        public static void main(String[] args)
+        {
+            // array of positive integers
+            int[] A = { 2, 6, 0, 9, 7, 3, 1, 4, 1, 10 };
+            int sum = 15;
+
+            findSubarray(A, sum);
+        }
+
 }
